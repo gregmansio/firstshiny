407400c69677c16213bc8706299b7c03c36bbc35
GrÃégoire Mansio <gregoire@mansio.ovh>
2021-05-19 20:16:40 +0200

    Pre-final version (to do: columns headers)

--- app.R
+++ app.R
@@ -14,53 +14,58 @@ library(shinythemes)
 library(tidyverse)
 library(DT)
 
-attach(mining)
 
 ##########----------------------------
 # Download and Reading of the file
 ##########----------------------------
+file_url <- "https://github.com/gregmansio/udemyshiny/raw/master/mining.RData"
+mining <- "./mining.RData"
+download.file(file_url, mining)
+load("./mining.RData")
 
+mining <- rename(mining, MarketCap = "MarketCap in M")
 
-csvudemy <- tempfile()
-download.file("https://att-c.udemycdn.com/2016-12-13_01-49-14-3df32750885cd4e0d78389c106e0ff30/original.csv?response-content-disposition=attachment%3B+filename%3Dcourse_proj_data.csv&Expires=1621437532&Signature=LuvBUJ8CIGw9sizalIUNr6DqJeDln3DDP-8NEtV2OvcHNHHhpCaO8DcwKyYc9kR6YPynh41vkBi~stJ1nASgs8p3hUzqI0WK2QA7bsN9jby-FHBMVAR719S8JenW8Mx7Cfv6n0A7N3VSzPh8ARkfxcPa6-xD6h4Dh-TlkzKYlm3Jmtk78J5zm~FHiPGSHH7q~T~wC1PorNxyPKewALt9LW9uUc~oCbR7--MWNjOqbJxb9~AWSNMT0RWV~q4zRERenUPU~IeIJw9c4~a9jktDB25KOPoI220Q6HUmVyo7JOGN7mCemvegimnns7EGrJwkJx4jFDwTXhCtTAHzE7MHXg__&Key-Pair-Id=APKAITJV77WS5ZT7262A", csvudemy)
-
-mining <- read_csv2(csvudemy)
+attach(mining)
 
 ##########----------------------------
 # Server
 ##########----------------------------
-server <- function(input, output, session) {
+server = function(input, output, session) {
   
   library(shiny)
   library(shinythemes)
   library(tidyverse)
   library(DT)
   
-  mycalculation <- sum(WG1*"G1", WG2*"G2", WG3*"G3")
+  mycalculation = reactive(
+    cbind(mining,
+          xcalc = input$WG1 * G1 + input$WG2 * G2 + input$WG3 * G3)
+    )
   
-  output$plot <- renderPlot({
-    ggplot(mining, aes(mycalculation,marketcap)) +
+  output$plot = renderPlot({
+    ggplot(mycalculation(), aes(xcalc,MarketCap)) +
+      geom_point() +
       geom_smooth(method="lm") +
       xlab("Value calculation result") +
       ylab("Market capitalisation in Million USD")
-  })
+    })
   
-  selection <- reactive({
+  selection = reactive({
     user_brush <- input$user_brush
-    sel <- brushedPoints(mining, user_brush)
+    sel <- brushedPoints(mycalculation(), user_brush)
     return(sel)
-  })
+    })
   
-  output$selectedtable <- DT::renderDataTable(DT::datatable(selection()))
-  output$rawtable <- DT::renderDataTable(DT::datatable(mining) %>%
-                                         formatCurrency("MarketCap in M", "$", digits=0) %>%
+  output$selectedtable = DT::renderDataTable(DT::datatable(selection()))
+  output$rawtable = DT::renderDataTable(DT::datatable(mining) %>%
+                                         formatCurrency("MarketCap", "$", digits=0) %>%
                                          formatStyle("Symbol", color = "grey") %>%
                                          formatStyle(c("G3", "G2", "G1"), backgroundColor = "lightblue")   
                                          )
   
   output$my_mining_download = downloadHandler(filename="my_mining_selection.csv",
                                               content = function(file) {
-                                                write.csv(selection, file)
+                                                write.csv(selection(), file)
                                               })
   output$full_mining_download = downloadHandler(filename="full_mining_table.csv",
                                                 content = function(file){
@@ -72,30 +77,32 @@ server <- function(input, output, session) {
 # UI
 ##########----------------
 
-ui <- navbarPage(theme = shinytheme("slate"),
-                 title = h3("Mining companies stocks"),
-                 tabPanel(("Stocks values explorer App"),
-                          wellPanel(
-                                    sliderInput(inputId = "WG1", label = "Weight on Grade 1", min = 0 , max = 20 , value = 8),
-                                    sliderInput(inputId = "WG2", label = "Weight on Grade 2", min = 0 , max = 20 , value = 8),
-                                    sliderInput(inputId = "WG3", label = "Weight on Grade 3", min = 0 , max = 6  , value = 2.4, step = 0.2),
-                                    ),
-                                    plotOutput("plot", brush = "user_brush"),
-                                    DT::dataTableOutput("selectedtable"),
-                                    downloadButton(outputId = "my_mining_download", label = "Download Table")
+ui = navbarPage(theme = shinytheme("slate"), title = h2("Mining companies stocks"),
+                 tabPanel(
+                   ("Stocks values explorer App"),
+                 wellPanel(
+                     sliderInput(inputId = "WG1", label = "Weight on Grade 1", value = 8, min = 0 , max = 20),
                                    
-                          ),
-                           
+                     sliderInput(inputId = "WG2", label = "Weight on Grade 2", value = 8, min = 0 , max = 20),
+                                   
+                     sliderInput(inputId = "WG3", label = "Weight on Grade 3", value = 2.4, min = 0 , max = 6, step = 0.2)
+                  ),
+                  plotOutput("plot", brush = "user_brush"),
+                  DT::dataTableOutput("selectedtable"),
+                  downloadButton(outputId = "my_mining_download", label = "Download Table")
+                 ),
+                                             
                  tabPanel("Documentation",
-                          "Documentation sourced from Youtube",
-                          tags$iframe(src="https://www.youtube.com/embed/vySGuusQI3Y"),
-                          style="height:900px, width:900px"
+                          h4("Documentation sourced from Youtube"),
+                          tags$iframe(style="height:900px, width:900px",
+                                      src="https://www.youtube.com/embed/vySGuusQI3Y")
+                          
                         ),
                            
                  tabPanel("Raw Data Table",
                                     DT::dataTableOutput("rawtable"),
-                                    downloadButton(outputId = "full_mining_download", label = "Download Table")
-                        ),
+                                    downloadButton(outputId = "full_mining_download", label = "Download Full Table")
+                        )
                 
                           
                 )
